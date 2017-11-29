baeb8c76db13274c5450d1d3b9b48b3ca80c0c5f
Paul <cyberprime99@gmail.com>
2017-11-29 15:47:27 +0100

    Basic Mapping
    Mapping the attributes of Contact and the Catalog.

--- ORM/ORM/Contact.cs
+++ ORM/ORM/Contact.cs
@@ -1,26 +1,33 @@
 ﻿using System;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
 using System.Text.RegularExpressions;
 
 namespace ORM
 {
-    class Contact : IComparable<Contact>
+    [Table (Name="Contacts")]
+    public class Contact : IComparable<Contact>
     {
+        [Column(IsPrimaryKey = true, IsDbGenerated = true)]
         private int id
         {
             get { return id; }
             set
             {
-                if (id > 10000 && id < 99999) id = value;
-                else;// Exception to do later
+                if (id > 0 && id < 99999) id = value;
+                else;//TODO Exception to do later
             }
         }
+        [Column]
         private string name
         {
             get { return name; }
             set { name = value.ToUpper(); }
         }
+        [Column]
         private string firstName;
 
+        [Column]
         private string mail
         {
             get { return mail; }
@@ -28,13 +35,16 @@ namespace ORM
             {
                 Regex regMail = new Regex(@"^[a-zA-Z0-9.-_]+@{1,1}[a-zA-Z0-9.-_]{2,}\.[a-zA-Z0-9.]{2,6}$");
                 if (regMail.IsMatch(value)) mail = value;
-                else; // THROW A EXCEPTION LATER
+                else; //TODO THROW A EXCEPTION LATER
             }
         }
 
+        [Column]
         private string address;
 
+        [Column]
         private DateTime dateOfBirth;
+
 
         public int CompareTo(Contact other)
         {
