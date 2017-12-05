cf81e0ef9b5572d847dedaa2a9fff6f5f196572f
Paul <cyberprime99@gmail.com>
2017-12-05 08:06:43 +0100

    Add graph + Starting Sql Server class + config file

---
+++ ORM/ORM/MServerSql.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ORM
+{
+    class MServerSql
+    {
+        private SqlConnection coSqlSer;
+        public SqlDataReader dtRdSql;
+        private SqlCommand cmd;
+        public string requete;
+        private string urlCon;
+
+        public void connectionSqlServ()
+        {
+            coSqlSer = new SqlConnection(urlCon);
+            coSqlSer.Open();
+        }
+        public void ExeDatabase()
+        {
+            cmd = new SqlCommand(requete, coSqlSer);
+            dtRdSql = cmd.ExecuteReader();
+        }
+
+        public void Disconnect()
+        {
+            dtRdSql.Dispose();
+            coSqlSer.Dispose();
+    }
+    }
+}
