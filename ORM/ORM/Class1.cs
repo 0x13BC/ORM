46549c333d3241efcff400f6c3f7c2fafe90b1b7
KarimGrandemange <karim.grandemange@ynov.com>
2017-12-12 23:54:10 +0100

    Update Class1.cs

--- ORM/ORM/Class1.cs
+++ ORM/ORM/Class1.cs
@@ -4,13 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Reflection;
+
 namespace ORM
 {
-    public class Class1
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class | AttributeTargets.Interface)]
+	internal class Class1
+		: Attribute
     {
-        public void test()
-        {
-
-        }
+        public bool ContinueOnCapturedContext { get; set; }
+		public Method Method { get; set; }
+		public bool ApplyToDescendents {get; set; }
+		
+		public test()
+		  : this(default(Method))
+		{
+		}
+	
+		public test(Method method)
+		{
+			this.Method = method;
+		}
     }
 }
