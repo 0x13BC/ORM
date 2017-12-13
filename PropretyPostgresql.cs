c0fdae469c011a95b7728604f2c67becbe77d786
djouma <32673976+djouma@users.noreply.github.com>
2017-12-13 11:55:59 +0100

    Add files via upload

---
+++ PropretyPostgresql.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM
+{
+   public class PropretyPostgresql
+    {
+        public string nom;
+        public string type;
+        public bool isNullable;
+        public bool isPrimaryKey;
+
+    }
+}
