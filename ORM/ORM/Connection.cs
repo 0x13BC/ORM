f47a4e338d56dd355610b4af353b1fb6bdaf510d
djouma <32673976+djouma@users.noreply.github.com>
2017-12-13 12:17:09 +0100

    Add files via upload

---
+++ ORM/ORM/Connection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+using Npgsql.PostgresTypes;
+using NpgsqlTypes;
+using System.Data;
+
+namespace ORM
+{
+    class Connection
+    {
+        public void ConnectionPostgreSql(string Connection)
+        {
+            var connString = "Host=myserver;Username=mylogin;Password=mypass;Database=mydatabase";
+
+            using (var conn = new NpgsqlConnection(connString))
+            {
+                conn.Open();
+            }
+        }
+    }
+}
